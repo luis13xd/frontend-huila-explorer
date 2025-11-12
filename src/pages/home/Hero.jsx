53645df47a9e37da7a9aca462530ef8cb662040e
Luis <trujillolopezluisxd@gmail.com>
2025-11-12 10:54:37 -0500

    mejjoramos el carrusel, ahora tambien sale imgs de sitios agregados

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -1,8 +1,11 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination, Autoplay } from "swiper/modules";
+import { useFetchBlogsQuery } from "../../redux/features/blogs/blogsApi";
+
+// Imágenes locales por defecto
 import Image1 from "../../assets/hero-carousel/img1.jpg";
 import Image2 from "../../assets/hero-carousel/img2.jpg";
 import Image3 from "../../assets/hero-carousel/img3.jpg";
@@ -11,67 +14,60 @@ import Image5 from "../../assets/hero-carousel/img5.jpg";
 import Image6 from "../../assets/hero-carousel/img6.jpg";
 
 const Hero = () => {
+  // ✅ Obtenemos los sitios desde el backend
+  const { data: blogs = [] } = useFetchBlogsQuery({});
+
+  // ✅ Unimos las imágenes locales + las de los sitios
+  const defaultImages = [Image1, Image2, Image3, Image4, Image5, Image6];
+  const siteImages = blogs
+    .filter(blog => blog.coverImg) // solo los que tengan imagen principal
+    .map(blog => blog.coverImg);
+
+  const allImages = [...defaultImages, ...siteImages]; // mezcla final
+
   return (
     <div className="flex flex-col md:flex-row justify-between items-center md:gap-14 gap-8">
+      {/* Texto */}
       <div className="md:w-1/2 w-full text-center">
         <h1 className="md:text-5xl text-3xl font-bold md:leading-tight">
           Descubre los negocios que hacen grande al Huila
         </h1>
         <p className="py-4">
           Explora lo mejor de nuestra comunidad, donde cada negocio cuenta una
-          historia de esfuerzo, innovación y tradición.{" "}
+          historia de esfuerzo, innovación y tradición.
         </p>
         <p className="font-medium">
           ¡Apoya lo local y encuentra de todo en el Huila!
         </p>
       </div>
 
+      {/* Carrusel */}
       <div className="md:w-1/2 w-full mx-auto">
         <Swiper
           slidesPerView={1}
           spaceBetween={1}
-          pagination={{
-            clickable: true,
-          }}
+          pagination={{ clickable: true }}
           autoplay={{
-            delay: 1000,
+            delay: 2000,
             disableOnInteraction: false,
           }}
           breakpoints={{
-            640: {
-              slidesPerView: 1,
-              spaceBetween: 10,
-            },
-            768: {
-              slidesPerView: 1,
-              spaceBetween: 20,
-            },
-            1024: {
-              slidesPerView: 1,
-              spaceBetween: 30,
-            },
+            640: { slidesPerView: 1, spaceBetween: 10 },
+            768: { slidesPerView: 1, spaceBetween: 20 },
+            1024: { slidesPerView: 1, spaceBetween: 30 },
           }}
           modules={[Pagination, Autoplay]}
           className="mySwiper"
         >
-          <SwiperSlide>
-            <img src={Image5} className="w-full lg:h-[420px] sm:h-96 h-80 " />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={Image6} className="w-full lg:h-[420px] sm:h-96 h-80 " />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={Image1} className="w-full lg:h-[420px] sm:h-96 h-80 " />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={Image2} className="w-full lg:h-[420px] sm:h-96 h-80 " />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={Image3} className="w-full lg:h-[420px] sm:h-96 h-80 " />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={Image4} className="w-full lg:h-[420px] sm:h-96 h-80 " />
-          </SwiperSlide>
+          {allImages.map((img, index) => (
+            <SwiperSlide key={index}>
+              <img
+                src={img}
+                alt={`slide-${index}`}
+                className="w-full lg:h-[420px] sm:h-96 h-80 object-cover rounded-xl"
+              />
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </div>
